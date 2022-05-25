50b51e56d40c09fccf3de6017bc7d487d1fa9b48
João Marcos <joaodias0078@gmail.com>
2022-05-24 22:25:52 -0400

    versão 1.8

--- lib/screens/schedules.dart
+++ lib/screens/schedules.dart
@@ -13,6 +13,9 @@ class _ScheduleAddScreenState extends State<ScheduleAddScreen> {
 
   late bool isButtonActive = true;
 
+  late bool val = false;
+
+  get onChanged => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,74 +25,104 @@ class _ScheduleAddScreenState extends State<ScheduleAddScreen> {
         centerTitle: true,
       ),
       body: Center(
-        child: Card(
-          child: Form(
-            child:Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      width: 300.0,
-                      child: DropdownButtonFormField<String>(
-                        hint: const Text('Selecionar Cliente'),
-                        items: valueList.map((String value) => 
-                        DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value)
-                        )
-                      ).toList(), 
-                        onChanged: (value){
-                          setState(() {
-                            if (value == value){
-                              isButtonActive = false;
-                            }else{
-                            value = value;
-                            }
-                          });
-                        } ,
-                      ),
-                    ),
-                    OutlinedButton(onPressed: isButtonActive
-                      ? (){
+        child: Form(
+          child:Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    width: 300.0,
+                    child: DropdownButtonFormField<String>(
+                      hint: const Text('Selecionar Cliente'),
+                      items: valueList.map((String value) => 
+                      DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value)
+                      )
+                    ).toList(), 
+                      onChanged: (value){
                         setState(() {
-                          isButtonActive = isButtonActive;
+                          if (value == value){
+                            isButtonActive = false;
+                          }else{
+                          value = value;
+                          }
                         });
-                    } : null, 
-                    child: const Text('Cadastrar'),)
-                  ],
+                      } ,
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: isButtonActive
+                    ? (){
+                      setState(() {
+                        isButtonActive = isButtonActive;
+                      });
+                  } : null, 
+                  child: const Text('Cadastrar'),),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0,20),
+                child: Text('Selecionar Serviços', 
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold, 
+                  color: Color.fromARGB(255, 3, 201, 108)
+                  ),
                 ),
-
-                Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      width: 400.0,
-                      child: DropdownButtonFormField<String>(
-                        hint: const Text('Selecionar Serviço'),
-                        items: serviceList.map((String value) => 
-                        DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value)
-                        )
-                      ).toList(), 
+              ),
+              SizedBox(
+                height: 300,
+                child: Expanded(
+                  flex: 10,
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return CheckboxListTile(
+                        title: Text('Hoje'),
+                        value: val, 
                         onChanged: (value){
                           setState(() {
-                            if (value == value){
-                              isButtonActive = false;
-                            }else{
-                            value = value;
-                            }
+                            val = value!;
                           });
-                        } ,
-                      ),
+                        }
+                      );
+                    }, 
+                    separatorBuilder: (BuildContext context, int index) { 
+                      return const Divider(height: 16,);
+                     },
+                    itemCount: 16,
+                  ),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    width: 300.0,
+                    child: DropdownButtonFormField<String>(
+                      hint: const Text('Selecionar Cliente'),
+                      items: valueList.map((String value) => 
+                      DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value)
+                      )
+                    ).toList(), 
+                      onChanged: (value){
+                        setState(() {
+                          if (value == value){
+                            isButtonActive = false;
+                          }else{
+                          value = value;
+                          }
+                        });
+                      } ,
                     ),
-                  ],
-                )
-
-              ],
-            ), 
-          ),
+                  ),
+                ],
+              )
+            ],
+          ), 
         ),
       ),
     );
